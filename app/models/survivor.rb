74f0797379cd6fddbbc2b79a7dc0fdebcb0c802c
Ana Beatriz <anabeatriz.frotac@gmail.com>
2022-06-22 04:03:21 -0300

    #main adding methods

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -3,7 +3,14 @@ class Survivor < ApplicationRecord
     validates :birthdate, presence: true, format: { without: /\A[a-zA-Z]+\z/,
                                                     message: 'only allows numbers' }
     validates :gender, presence: true, length: { maximum: 10 }
-    validates :latitude, presence: true, length: { maximum: 3 }
-    validates :longitude, presence: true, length: { maximum: 3 }
+    validates :latitude, presence: true, length: { maximum: 10 }
+    validates :longitude, presence: true, length: { maximum: 10 }
     validates :reports, numericality: { only_integer: true }, allow_blank: true
-end
+
+    def self.report_abducted
+        (Survivor.where(abducted: true).size / Survivor.all.size) * 100
+      end
+    
+      def self.report_non_abducted
+        (Survivor.where(abducted: false).size / Survivor.all.size) * 100
+      end
